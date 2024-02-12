e4ea78e026674915e716a1fa28737f38b3ce00c8
hamzademirr <demha082@gmail.com>
2024-02-12 15:04:09 +0300

    feat edit footer

--- todo-app/src/components/Footer.jsx
+++ todo-app/src/components/Footer.jsx
@@ -4,7 +4,8 @@ function Footer() {
   return (
     <footer className="info">
       <p>Click to edit a todo</p>
-      <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+      <p>Created by <a href="https://github.com/hamzademirr">Hamza D.</a></p>
+      <p>Desing by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
       <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
     </footer>
   )
